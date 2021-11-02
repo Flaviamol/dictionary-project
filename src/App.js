8bf4f3a729a62d770bde0f514d8f558ad51bd61e
Flaviamol <flaviacm.de@gmail.com>
2021-11-02 15:19:50 +0100

    inicial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Dictionary</h1>
+        <h3>What word do you want to look up?</h3>
+      </header>
+    </div>
+  );
+}
+
+export default App;
