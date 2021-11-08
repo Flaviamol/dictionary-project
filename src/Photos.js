06f37564cd86a342a23aa8c45039d124901655f1
Flaviamol <flaviacm.de@gmail.com>
2021-11-08 17:32:47 +0100

    photos line up and down

--- src/Photos.js
+++ src/Photos.js
@@ -5,17 +5,19 @@ export default function Photos(props){
         console.log(props.photos);
       return (
           <div className="Photos">
+              <hr className="top-line"/>
               <div className="row">
                 {props.photos.map(function(photo, index){
                     return (
                         <div className="col-4" key={index}>
                             <a href={photo.src.original} target="_blank" rel="noreferrer">
-                              <img src={photo.src.landscape} alt="" className="img-fluid" />    
+                                <img src={photo.src.landscape} alt="" className="img-fluid" />    
                             </a>
-                        </div>
+                        </div> 
                     )
                 })}  
               </div>
+              <hr className="bottom-line"/>
           </div>
       )  
     } else {
